241810ea6015c5351ad474e7e379828717ca7e18
Gary Powell-Jones <g.powell-jones@sharepointresource.co.uk>
2018-11-15 13:59:17 +0000

    copied from local lapotop - should be working

---
+++ README.md
@@ -0,0 +1,2 @@
+# SharePoint Provisioning
+This project is intended to provide provisioning extensions to PowerShell and the PnP framework
---
+++ SPR.Provisioning/Helpers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core.Framework.Provisioning.Extensibility;
+using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using OfficeDevPnP.Core.Framework.Provisioning.ObjectHandlers;
+using OfficeDevPnP.Core.Framework.Provisioning.Connectors;
+using System.IO;
+
+namespace SPR.Provisioning
+{
+    class Helpers
+    {
+        private const string workflowFolderName = "NintexWorkflows";
+
+        public void DownloadWorkflowFile(string workflowID, FileConnectorBase writer, Stream fileStream, WorkflowLevel workflowLevel)
+        {
+            writer.SaveFileStream(workflowID + ".xml", workflowFolderName, fileStream);
+        }
+        public string ReadWorkflowFile(string fileName, FileConnectorBase fileReader)
+        {
+
+            //FileSystemConnector connector = new FileSystemConnector(null,workflowFolderName);
+
+            //string fileContents = connector.GetFile(fileName);
+            string fileContents = fileReader.GetFile(fileName);
+            return fileContents;
+        }
+    }
+}
